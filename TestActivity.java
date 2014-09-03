25506e61dd466e64e530def668c24dbf13ef1644
getBoostify <info@getboostify.com>
2014-09-03 17:50:38 +0300

    Update TestActivity.java

--- TestActivity.java
+++ TestActivity.java
@@ -1,20 +1,2 @@
-
-public class TestActivity extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.test_layout);
-        
-        //init Boostify
-		BoostifyManager.getInstance(this).initBoostify("BOOSTIFY_APP_ID");
-		
-		((Button) findViewById(R.id.test)).setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-                //show Boostify wall
-				BoostifyManager.getInstance(TestActivity.this).showBoostifyWall();
-			}
-		});
-		
-}
+BoostifyManager.getInstance(this).initBoostify("BOOSTIFY_APP_ID");
+BoostifyManager.getInstance(TestActivity.this).showBoostifyWall();
